7931a03757930df2c5364ad467000a2fa32d8057
Lanselott <497653881@qq.com>
2019-01-07 15:21:19 +0800

    upload jpg

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 header('Content-type: image/jpeg');
 
 $image = new Imagick('image.jpg');
-
+$echo("hello");
 // If 0 is provided as a width or height parameter,
 // aspect ratio is maintained
 $image->setbackgroundcolor('rgb(64, 64, 64)');
