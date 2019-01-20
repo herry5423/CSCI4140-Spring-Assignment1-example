47cf5f9c460799876d6447c4257752c4dbce6b95
Lanselott <497653881@qq.com>
2019-01-21 01:44:45 +0800

    add imagick ext

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<html>
+
+<body>
 <?php 
 $url = "https://webinstagram.s3.amazonaws.com/image.jpg";
-echo $url;
-$image = file_get_contents($url);
+#$image = file_get_contents($url);
 
-header('Content-type: image/png'); 
-echo '<img src="'.$url.'">';
+#header('Content-type: image/png'); 
+#echo '<img src="'.$url.'">';
 ?>
+<p>
+	<img src="<?php echo $url ?>" >
+</p>
+
+</body>
+</html>
