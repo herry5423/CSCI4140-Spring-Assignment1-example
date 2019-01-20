57a2b5083e9cd0090295fb42c4cefdb85995970d
Lanselott <497653881@qq.com>
2019-01-20 22:24:50 +0800

    add imagick ext

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
- 		error_log($image = new Imagick());
- 		$image->newImage(100,100,new ImagickPixel('red'));
- 		$image->setImageFormat('png');
- 		
-		echo $image;
-        /*** a file that does not exist ***/
-        #$url = "https://webinstagram.s3.amazonaws.com/image.jpg";
-        #echo $url;
-        #$image = file_get_contents($url);
- 		?>			
+<?php 
+$image = new Imagick(); 
+$image->newImage(100, 100, new ImagickPixel('red')); 
+$image->setImageFormat('png'); 
+
+header('Content-type: image/png'); 
+echo $image; 
+?>
