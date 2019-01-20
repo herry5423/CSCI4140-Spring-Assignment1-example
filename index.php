3dfe90deb921dd4ac635c0f523e1b0ea1eff1ec1
Lanselott <497653881@qq.com>
2019-01-20 22:22:28 +0800

    add imagick ext

--- index.php
+++ index.php
@@ -1,30 +1,11 @@
 <?php
-require('vendor/autoload.php');
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = Aws\S3\S3Client::factory();
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-?>
-<html>
-    <head><meta charset="UTF-8"></head>
-    <body>
-        <h1>S3 upload example</h1>
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-    // FIXME: add more validation, e.g. using ext/fileinfo
-    try {
-        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
-        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
-?>
-        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
-<?php } catch(Exception $e) { ?>
-        <p>Upload error :(</p>
-<?php } } ?>
-        <h2>Upload a file</h2>
-        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-            <input name="userfile" type="file"><input type="submit" value="Upload">
-        </form>
-
- 	<img src="show_image.php" />
-
-    </body>
-</html>
+ 		error_log($image = new Imagick());
+ 		$image->newImage(100,100,new ImagickPixel('red'));
+ 		$image->setImageFormat('png');
+ 		
+		echo $image;
+        /*** a file that does not exist ***/
+        #$url = "https://webinstagram.s3.amazonaws.com/image.jpg";
+        #echo $url;
+        #$image = file_get_contents($url);
+ 		?>			
