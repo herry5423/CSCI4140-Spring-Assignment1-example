19783e7a4f3dd6487f5fe6a6c1fb6bfbc5f3ee78
Lanselott <497653881@qq.com>
2019-01-21 15:50:30 +0800

    update

--- index.php
+++ index.php
@@ -18,25 +18,24 @@ $url = "https://webinstagram.s3.amazonaws.com/image.jpg";
 	$image = file_get_contents($url);
 	$img = new Imagick();
 	$img -> readImageBlob($image);	
-	$img -> resizeImage(320,240,Imagick::FILTER_LANCZOS,1);
+	
+	$matrix = [
+        [-1, 0, -1],
+        [0,  5,  0],
+        [-1, 0, -1],
+    ];
+     
+    $kernel = ImagickKernel::fromMatrix($matrix);
+    $strength = 0.5;
+    $kernel->scale($strength, Imagick::NORMALIZE_KERNEL_VALUE);
+    $kernel->addUnityKernel(1 - $strength);
+
+    $img->filter($kernel);
+	#$img -> resizeImage(320,240,Imagick::FILTER_LANCZOS,1);
     // header("Content-Type: image/png");
     echo '<img src="data:image/jpg;base64,'.base64_encode($img->getImageBlob()).'" alt="" />';
     // echo $img->getImageBlob();
 ?>
-
-<?php 
-	$image = file_get_contents($url);
-	$im = new Imagick ();
-	$img -> readImageBlob($image);
-	#echo $img -> writeImage('/app/images/tempimg.jpg');
-	echo exec("wget https://webinstagram.s3.amazonaws.com/image.jpg | mv image.jpg /tmp/temp2.jpg");
-	$in = "/tmp/temp2.jpg";
-	$iout = "/app/images/temp.jpg";
-	unlink($iout);
-	echo exec("/usr/bin/convert $in -resize 50% $iout");	
-	// echo '<img src="data:image/jpg;base64,'.base64_encode($imgg->getImageBlob()).'" alt="" />';
-?>
-<div align="center"><img src="<? echo $in; ?>" /></div>
 
 </body>
 </html>
