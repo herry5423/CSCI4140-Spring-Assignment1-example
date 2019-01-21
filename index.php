644cbca01ed4cf4444a3a8ee5f4d1705937fc07d
Lanselott <497653881@qq.com>
2019-01-21 10:44:38 +0800

    update

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $url = "https://webinstagram.s3.amazonaws.com/image.jpg";
 	$image = file_get_contents($url);
 	$im = new Imagick ();
 	$img -> readImageBlob($image);
-	echo $img -> writeImage('/app/temp/tempimg.jpg');
+	echo $img -> writeImage('/tmp/tempimg.jpg');
 	$in = "/app/images/image.jpg";
 	$iout = "/app/images/temp.jpg";
 	unlink($iout);
