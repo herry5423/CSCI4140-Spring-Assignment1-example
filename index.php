27897d0898c7f05dd44960a78255ecfb3eae6911
Lanselott <497653881@qq.com>
2019-01-27 16:44:26 +0800

    update

--- index.php
+++ index.php
@@ -16,18 +16,9 @@ $url = "https://webinstagram.s3.amazonaws.com/image.jpg";
 </p>
 
 <?php 
-// Set handler to overide SESSION  
-session_set_save_handler(  
-array($this, "_open"),  
-array($this, "_close"),  
-array($this, "_read"),  
-array($this, "_write"),  
-array($this, "_destroy"),  
-array($this, "_gc")  
-); 
 
 // Start the session  
-session_start();  
+// session_start();  
 
 
 
